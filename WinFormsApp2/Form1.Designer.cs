b3b7cd94a6e690719fec231f95b8b021735feb3b
khalin1337 <84382013+khalin1337@users.noreply.github.com>
2024-06-02 13:10:20 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lockableButton1 = new WinFormsControlLibrary2.LockableButton();
+            SuspendLayout();
+            // 
+            // lockableButton1
+            // 
+            lockableButton1.Location = new Point(281, 183);
+            lockableButton1.MaxClicks = 5;
+            lockableButton1.Name = "lockableButton1";
+            lockableButton1.Size = new Size(150, 50);
+            lockableButton1.TabIndex = 0;
+            lockableButton1.UnlockTime = 5000;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lockableButton1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private WinFormsControlLibrary2.LockableButton lockableButton1;
+    }
+}
---
+++ WinFormsControlLibrary1/UserControl1.Designer.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+
+namespace WinFormsControlLibrary1
+{
+        [ToolboxBitmap(typeof(Button))]
+        public partial class LockableButton : UserControl
+        {
+            private Button button;
+            private System.Windows.Forms.Timer unlockTimer;
+            private int clickCount = 0;
+            private int maxClicks = 5;  // Максимальна кількість натискань до блокування
+            private int unlockTime = 5000;  // Час розблокування у мілісекундах
+
+            [Category("Behavior")]
+            [Description("Maximum number of clicks before the button is locked.")]
+            public int MaxClicks
+            {
+                get { return maxClicks; }
+                set { maxClicks = value; }
+            }
+
+            [Category("Behavior")]
+            [Description("Time in milliseconds before the button is unlocked.")]
+            public int UnlockTime
+            {
+                get { return unlockTime; }
+                set { unlockTime = value; }
+            }
+
+            public LockableButton()
+            {
+                InitializeComponent();
+
+                button = new Button();
+                button.Dock = DockStyle.Fill;
+                button.Click += Button_Click;
+                this.Controls.Add(button);
+
+                unlockTimer = new System.Windows.Forms.Timer();
+                unlockTimer.Tick += UnlockTimer_Tick;
+            }
+
+            private void Button_Click(object sender, EventArgs e)
+            {
+                clickCount++;
+                if (clickCount >= maxClicks)
+                {
+                    LockButton();
+                    unlockTimer.Interval = unlockTime;
+                    unlockTimer.Start();
+                }
+            }
+
+            private void LockButton()
+            {
+                button.Enabled = false;
+                // Можна додати логіку для візуального позначення заблокованої кнопки
+            }
+
+            private void UnlockButton()
+            {
+                button.Enabled = true;
+                clickCount = 0;
+                // Можна додати логіку для візуального позначення розблокованої кнопки
+            }
+
+            private void UnlockTimer_Tick(object sender, EventArgs e)
+            {
+                unlockTimer.Stop();
+                UnlockButton();
+            }
+        }
+    }
